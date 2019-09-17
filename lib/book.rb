5f9ce625fdd7323da31fd69a26a531701dfeab95
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-17 12:24:24 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   attr_accessor :author, :page_count
-  attr_reader :title
+  attr_reader :title, :genre 
   
   GENRES = []
 
@@ -14,10 +14,7 @@ class Book
   
   def genre=(genre)
     @genre = genre 
-  end 
-  
-  def genre 
-    @genre 
+    GENRES << genre 
   end 
 
 end
