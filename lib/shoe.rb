9f88dabf1cd8a4f2441b9de42fc3c443f8851741
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-17 12:29:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,12 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
+  def initialize(brand)
+    @brand = brand 
+  end 
+  
   BRANDS = []
-
-def brand=(brand)
-  @brand = brand 
-  BRANDS << brand.unique 
-end 
     
 
   def cobble
